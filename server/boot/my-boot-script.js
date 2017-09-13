8c536fed727a7c6dc9973426a56b40d1aa6d9fa7
mopics25@gmail.com <mopics25@gmail.com>
2017-09-13 17:25:45 +0200

    DONE! And that's how a puppy takes a crap takes one look at it and moves ON

--- server/boot/my-boot-script.js
+++ server/boot/my-boot-script.js
@@ -5,17 +5,4 @@ module.exports = function(app) {
   });
   var Email = app.models.Email;
 
-  /*Email.send({
-      to: 'peter@mopics.nl',
-      from: 'mopics25@gmail.com',
-      subject: 'my subject',
-      text: 'my text',
-      html: 'my <em>html piss on yer head!!</em>'
-    }, function(mail) {
-      console.log('email sent!');
-    },
-    function(res){
-      console.log( "Email Error:"+ res.status+" "+res.statusText );
-    }
-  );*/
 }
